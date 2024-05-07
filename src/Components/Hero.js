b7275032774c6b1853d97971165b7ae5511df8ca
AashishSingh19 <ashishlalsingh19@gmail.com>
2024-05-07 16:28:05 +0545

    updated header and button

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import image from '../Assets/spiderman.jpg'
 
 export default function Hero() {
   return (
     <>
-    <div className="text-white">
+    <div className="mt-20">
+        <img 
+        className="w-200"
+        src={image} 
+        alt="" />
     </div>
     </>
   )
