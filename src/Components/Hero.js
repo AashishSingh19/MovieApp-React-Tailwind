9fafe8989b66a23877e8c92a1b95e8c7145400e3
AashishSingh19 <ashishlalsingh19@gmail.com>
2024-05-15 16:43:41 +0545

    updated hero

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -4,12 +4,15 @@ import image from '../Assets/spiderman.jpg'
 export default function Hero() {
   return (
     <>
-    <div className="mt-20">
-        <img 
-        className="w-200"
-        src={image} 
-        alt="" />
+<div class="grid grid-cols-2 gap-4">
+    <div>
+        <img class="w-200" src={image} alt="" />
     </div>
+    <div>
+        <h1 className="text-white">Spiderman</h1>
+    </div>
+</div>
+
     </>
   )
 }
--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Movies() {
   return (
     <>
-    <h1>Movies</h1>
+    <h1 className="text-white">Movies</h1>
     </>
   )
 }
